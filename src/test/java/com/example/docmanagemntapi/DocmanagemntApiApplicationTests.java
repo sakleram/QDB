611bf211f880da99351284323a301eb78197fb46
sakleram <saklemca2005@gmail.com>
2023-08-06 16:06:57 +0530

    qdb assig commit

--- src/test/java/com/example/docmanagemntapi/DocmanagemntApiApplicationTests.java
+++ src/test/java/com/example/docmanagemntapi/DocmanagemntApiApplicationTests.java
@@ -2,25 +2,20 @@ package com.example.docmanagemntapi;
 
 import com.example.docmanagemntapi.controller.PdfController;
 import com.example.docmanagemntapi.controller.PostController;
-import com.sun.source.tree.AssertTree;
-import org.apache.coyote.Response;
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-
-import java.net.http.HttpResponse;
-
+import org.springframework.mock.web.MockMultipartFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DocmanagemntApiApplicationTests {
 
-	@Mock
+    @Mock
     private PdfController pdfController;
 
-	@Mock
+    @Mock
     private PostController postController;
 
 	@Test
@@ -34,6 +29,23 @@ class DocmanagemntApiApplicationTests {
         postController.viewPost("1");
         assertEquals("200 OK", HttpStatus.OK.toString());
     }
+    @Test
+    public void createProducts(){
+	    String documentFileName = "FBPPolicy.pdf";
+        postController.createProducts(documentFileName);
+        assertEquals("200 OK", HttpStatus.OK.toString());
+    }
 
+    @Test
+    public void uploadFile() throws Exception {
+        MockMultipartFile firstFile = new MockMultipartFile("data", "FBPPolicy.pdf", "pdf/plain", "some xml".getBytes());
+        pdfController.uploadFile(firstFile);
+    }
+
+    @Test
+    public void viewPdf(){
+        String documentFileName = "FBPPolicy.pdf";
+        pdfController.viewPdf(documentFileName);
+    }
 
 }
